2a52f635012fb7ea3ce9c396202deb71355522f8
Nishanth Sura <Nishanth Sura@192.168.1.113>
2020-01-20 17:48:18 -0500

    Math Package

---
+++ nishanth/src/maths/Add.java
@@ -0,0 +1,13 @@
+package maths;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 12;
+		int b = 14;
+		System.out.println(a+b);
+
+	}
+
+}
---
+++ nishanth/src/maths/Sub.java
@@ -0,0 +1,13 @@
+package maths;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 12;
+		int b = 14;
+		System.out.println(b-a);
+
+	}
+
+}
